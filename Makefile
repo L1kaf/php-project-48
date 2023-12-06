990085a9407c0bcb870d41a4e3d4eaaac4f578cd
Pavel Buryka <l1kaf@yandex.ru>
2023-12-06 12:18:16 +0300

    update DifferTest.php, Makefile, phpunit.xml, main,yml

--- Makefile
+++ Makefile
@@ -1,5 +1,14 @@
 install:
 	composer install
 
+validate:
+	composer validate
+
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+
+test:
+	composer exec --verbose phpunit tests
+
+test-coverage:
+	composer exec --verbose phpunit tests -- --coverage-clover build/logs/clover.xml
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -29,6 +29,32 @@ Class DifferTest extends TestCase
             . "    + verbose: true" . PHP_EOL
             . "}" . PHP_EOL;
 
+        $expected2 = "{" . PHP_EOL
+            . "    + follow: false" . PHP_EOL
+            . "      host: hexlet.io" . PHP_EOL
+            . "    + proxy: 123.234.53.22" . PHP_EOL
+            . "    - timeout: 20" . PHP_EOL
+            . "    + timeout: 50" . PHP_EOL
+            . "    - verbose: true" . PHP_EOL
+            . "}" . PHP_EOL; 
+            
+        $expected3 = "{" . PHP_EOL
+            . "    - follow: false" . PHP_EOL
+            . "    - host: hexlet.io" . PHP_EOL
+            . "    - proxy: 123.234.53.22" . PHP_EOL
+            . "    - timeout: 50" . PHP_EOL
+            . "}" . PHP_EOL;
+
+        $expected4 = "{" . PHP_EOL
+            . "    + follow: false" . PHP_EOL
+            . "    + host: hexlet.io" . PHP_EOL
+            . "    + proxy: 123.234.53.22" . PHP_EOL
+            . "    + timeout: 50" . PHP_EOL
+            . "}" . PHP_EOL; 
+
         $this->assertEquals($expected1, genDiff($file1, $file2));
+        $this->assertEquals($expected2, genDiff($file2, $file1));
+        $this->assertEquals($expected3, genDiff($file1, $file3));
+        $this->assertEquals($expected4, genDiff($file3, $file1));
     }
 }
