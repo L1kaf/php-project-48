74cf1fb0327ecb23afc04f21e67813213c4fcd4a
Pavel Buryka <l1kaf@yandex.ru>
2023-12-11 12:19:13 +0300

    add json test

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -16,30 +16,18 @@ Class DifferTest extends TestCase
 
     public function testDiffer(): void
     {
-        $fileOneJson = $this->getFixtureFullPatch('file1.json');
-        $fileTwoJson = $this->getFixtureFullPatch('file2.json');
-        $fileOneYml = $this->getFixtureFullPatch('file1.yml');
-        $fileTwoYml = $this->getFixtureFullPatch('file2.yml');
-        $fileOneYaml = $this->getFixtureFullPatch('file1.yaml');
-        $fileTwoYaml = $this->getFixtureFullPatch('file2.yaml');
-        $fileThreeJson = $this->getFixtureFullPatch('file3.json');
-        $fileFourJson = $this->getFixtureFullPatch('file4.json');
-        $fileThreeYaml = $this->getFixtureFullPatch('file3.yaml');
-        $fileFourYaml = $this->getFixtureFullPatch('file4.yaml');
+        $fileOneJson = $this->getFixtureFullPatch('file3.json');
+        $fileTwoJson = $this->getFixtureFullPatch('file4.json');
+        $fileOneYaml = $this->getFixtureFullPatch('file3.yaml');
+        $fileTwoYaml = $this->getFixtureFullPatch('file4.yaml');
 
-        $expected1 = file_get_contents($this->getFixtureFullPatch('TestFlatCompare1.txt'));
-        $expected2 = file_get_contents($this->getFixtureFullPatch('TestFlatCompare2.txt'));
-        $expected3 = file_get_contents($this->getFixtureFullPatch('TestRecursiveCompare.txt'));
-        $expected4 = file_get_contents($this->getFixtureFullPatch('TestPlainCompare.txt'));
+        $expected1 = file_get_contents($this->getFixtureFullPatch('TestRecursiveCompare.txt'));
+        $expected2 = file_get_contents($this->getFixtureFullPatch('TestPlainCompare.txt'));
+        $expected3 = file_get_contents($this->getFixtureFullPatch('TestJsonCompare.txt'));
 
-        $this->assertEquals($expected1, genDiff($fileOneJson, $fileTwoJson));
-        $this->assertEquals($expected2, genDiff($fileTwoJson, $fileOneJson));
-        $this->assertEquals($expected1, genDiff($fileOneYml, $fileTwoYml));
-        $this->assertEquals($expected2, genDiff($fileTwoYml, $fileOneYml));
+        $this->assertEquals($expected1, genDiff($fileOneJson, $fileTwoJson ));
         $this->assertEquals($expected1, genDiff($fileOneYaml, $fileTwoYaml));
-        $this->assertEquals($expected2, genDiff($fileTwoYaml, $fileOneYaml));
-        $this->assertEquals($expected3, genDiff($fileThreeJson, $fileFourJson));
-        $this->assertEquals($expected3, genDiff($fileThreeYaml, $fileFourYaml));
-        $this->assertEquals($expected4, genDiff($fileThreeYaml, $fileFourYaml, "plain"));
+        $this->assertEquals($expected2, genDiff($fileOneJson, $fileTwoJson, "plain"));
+        $this->assertEquals($expected3, genDiff($fileOneJson, $fileTwoJson, "json"));
     }
 }
